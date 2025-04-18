d4dc2d40318dfd7244ba161bc7f1d1e3994be558
Caio Barros <caiobarros0312@gmail.com>
2025-04-18 12:05:17 -0300

    Revert "Alterando para 1"
    This reverts commit 149568696f6bb39bf0cbe5169228188d82e099a5.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 alert('Boas vindas ao jogo do número secreto');
 console.log("Mensagem de inicio exibida");
-let numeroSecreto = 1;
+let numeroSecreto = 10000;
 console.log(`Numero secreto é `+ numeroSecreto);
 let chute = prompt('Escolha um número entre 1 e 10');
 console.log(`Usuário escolheu `+ chute);
