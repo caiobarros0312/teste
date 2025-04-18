0ed0cbe3c60c0c0574dcf68f235b47eb8bb11dc1
Caio Barros <caiobarros0312@gmail.com>
2025-04-17 21:08:32 -0300

    primiero commit

---
+++ app.js
@@ -0,0 +1,16 @@
+alert('Boas vindas ao jogo do número secreto');
+console.log("Mensagem de inicio exibida");
+let numeroSecreto = 10;
+console.log(`Numero secreto é `+ numeroSecreto);
+let chute = prompt('Escolha um número entre 1 e 10');
+console.log(`Usuário escolheu `+ chute);
+if (chute == numeroSecreto) {
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+    console.log(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else if(chute < numeroSecreto){
+    alert('É maior');
+    console.log('É maior');
+}else {
+alert('É menor');
+console.log('É menor');
+}
